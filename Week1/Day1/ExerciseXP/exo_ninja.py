a32d703573927ec40fe99ee1b35adcc0290656d7
y-fares <yacine.fares.pro@gmail.com>
2025-10-03 14:49:09 +0200

    Mise en place du Git Flow

---
+++ Week1/Day1/ExerciseXP/exo_ninja.py
@@ -0,0 +1,16 @@
+print(3 <= 3 < 9)
+print(3 == 3 == 3)
+print(bool(0))
+print(bool(5 == "5"))
+print(bool(4 == 4) == bool("4" == "4"))
+print(bool(bool(None)))
+
+x = (1 == True)
+y = (1 == False)
+a = True + 4
+b = False + 10
+
+print("x is", x)
+print("y is", y)
+print("a:", a)
+print("b:", b)
